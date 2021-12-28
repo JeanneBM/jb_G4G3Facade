89b002e6e0e28703e3456f0ef26db389944ea35c
JeanneBM <JeanneBM@gmail.com>
2021-12-28 14:52:54 +0100

    Final

---
+++ src/pattern/DeutscheWelcome.java
@@ -0,0 +1,11 @@
+package pattern;
+
+    public class DeutscheWelcome implements interfejs {
+
+        @Override
+        public void welcomeMessage() {
+            System.out.println("Guten Tag! Ich heiße Asia. Auf Wiedersehen!");
+        }
+    }
+
+
